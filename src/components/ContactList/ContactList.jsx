4e3b6108dad04277b090b9c27063b0e3366cf8a4
vika59216 <viktoriiadupreez@gmail.com>
2024-06-18 16:38:06 +0200

    hw7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,20 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+
+import { selectFilteredContacts } from "../../redux/contactsSlice";
+//import { selectContacts } from "../../redux/contactsSlice";
+//import { selectNameFilter } from "../../redux/filtersSlice";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const filterContacts = contacts.filter((contact) =>
-  contact.name.toLowerCase().includes(filter.toLowerCase())
-);
+  //const contacts = useSelector(selectContacts);
+  //const filter = useSelector(selectNameFilter);
+  //const filterContacts = contacts.filter((contact) =>
+   // contact.name.toLowerCase().includes(filter.toLowerCase())
+    
+    
+  const filterContacts = useSelector(selectFilteredContacts);
+
 
   return (
     <ul className={css.personList}>
